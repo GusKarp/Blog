8b52116ef8fa0c4f63537ec6b13b920dbfa62627
GusKarp <GusKarp>
2016-04-01 00:54:12 -0700

    finished video 9: fixing errors in next  video, not sure what I'm doing

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,7 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+S(function () {
+    var APPLICATION_ID = "291AF3D8-BFD8-D246-FFEC-B57EC2F81200",
+        SECRET_KEY = "58A3D718-B1CD-308E-FF5E-2C57691F4300",
+        VERSION = "v1";
+        
+        Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+        
+        var user = new Backendless.user();
+        user.mail = "augustus.karp@yahoo.com";
+        user.password = "12345";
+        Backendless.userService.register(user);
+});
 
