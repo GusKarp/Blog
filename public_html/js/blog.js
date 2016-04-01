d5933034a0534da0b57c41d6acbb31a67ee70e01
GusKarp <GusKarp>
2016-04-01 09:06:35 -0700

    finished video 11: added post

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,13 +1,21 @@
-S(function () {
+/* global Backendless, Posts */
+
+$(function () {
     var APPLICATION_ID = "291AF3D8-BFD8-D246-FFEC-B57EC2F81200",
         SECRET_KEY = "58A3D718-B1CD-308E-FF5E-2C57691F4300",
         VERSION = "v1";
         
-        Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
         
-        var user = new Backendless.user();
-        user.mail = "augustus.karp@yahoo.com";
-        user.password = "12345";
-        Backendless.userService.register(user);
+    var dataStore = Backendless.Persistence.of(Posts);
+    var post = new Posts({title: "My First Blog Post", content:"My first blog post content", email:"email@email.com"});
+    dataStore.save(post);
 });
+
+function Post(args){
+    args = args || {};
+    this.title = args.title || "";
+    this.content = args.context || "";
+    this.authorEmail = args.emailEmail || "";
+}
 
